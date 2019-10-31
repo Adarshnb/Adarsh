1b844c87ebb9c9b883dc0ed38d5657a1012cffd1
admin <admin@admin-PC>
2019-10-31 12:49:10 +0530

    hi

---
+++ MyJavaProject/src/com/primitivetypecasting/Demo1.java
@@ -0,0 +1,18 @@
+package com.primitivetypecasting;
+
+public class Demo1 {
+
+	public static void print(double d)
+	{
+		System.out.println(d);
+	}
+	public static void main (String[] args) {
+		print(10);
+		print(10.0);
+		print('a');
+		
+	}
+		
+
+	}
+
